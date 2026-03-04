75b57a447bb39673abee9b0c69e0bbfb315eec9b
khalid192 <karazani2003@gmail.com>
2026-03-04 22:04:00 +0000

    .

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const joi = require("joi");
+
 const app = express();
 
 app.use(express.json());
@@ -36,26 +38,31 @@ app.get("/:id", (req, res) => {
   }
 });
 app.post("/", (req, res) => {
-  if (!req.body.name || req.body.name < 3) {
-    return res
-      .status(400)
-      .json("name is required and must be more than 3 characters");
-  } else if (!req.body.actor || req.body.actor < 3) {
-    return res
-      .status(400)
-      .json("actor is required and must be more than 3 characters");
-  } else {
+
+   const schema = joi.object({
+   name:joi.string().trim().min(3).max(200).required(),
+   actore:joi.string().trim().min(3).max(10).required(),
+   price:joi.number().min(0).required(),
+   cover:joi.string().trim().required(),
+   });
+
+   const {error} = schema.validate(req.body);
+
+   if (error){
+    return res.status(400).json({message:error.details[0].message})
+   }
     const book = {
       id: books.length + 1,
       name: req.body.name,
       actor: req.body.actor,
       price: req.body.price,
+      cover:req.body.cover,
     };
     books.push(book);
     res.status(201).send({ message: "bock added secsfuly", book });
-  }
+  
 });
 
 app.listen(5000, () => {
-  console.log("Server running....");
+  console.log("Server running...");
 });
