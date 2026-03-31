a544b98930fbdec5f5c03b25530ff9534e83c7ad
khalid192 <karazani2003@gmail.com>
2026-03-31 20:51:38 +0100

    0

--- routes/editePassword.js
+++ routes/editePassword.js
@@ -3,30 +3,56 @@ const router = express.Router();
 const { User } = require("../models/User.js");
 
 const { verifyTokenForUpload } = require("../middlewares/verifyToken");
+const bcrypt = require("bcryptjs");
 
-
-
-
-router.get("/editPassword", verifyTokenForUpload, async (req, res) => {   
+router.get("/editPassword", verifyTokenForUpload, async (req, res) => {
   const user = await User.findById(req.user.id);
   if (!user) {
     return res.redirect("/api/auth/login");
-  }  
-  res.render("editPassword", { user });
-} );
+  }
+  res.render("editPassword", { user, message: null });
+});
 
-router.post("/editPassword", verifyTokenForUpload, async (req, res) => { 
-   const user = await User.findById(req.user.id); 
-  const input1 = req.body.newPassword 
-  const input2 = req.body.confirmPassword 
- if (input1==input2){
-  user.password = input1
-  user.save()
- }else{
-  console.log(111)
- }
- const message = 'new password and confirm password do not match'
- res.render("editPassword", { user , message });
-} );
+router.post("/editPassword", verifyTokenForUpload, async (req, res) => {
+  const user = await User.findById(req.user.id);
+  const newPassword = req.body.newPassword;
+  const confirmPassword = req.body.confirmPassword;
+  const currentPassword = req.body.currentPassword;
+  const message = null;
 
-module.exports = router;                        
+  if (newPassword == confirmPassword) {
+    
+    if (!user.password) {
+      //update the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+      await user.save();
+
+      const message = "password updated successfully";
+      res.render("editPassword", { user, message });
+    } else {
+      const match = await bcrypt.compare(currentPassword, user.password);
+      if (!match) {
+        const message = "current password is incorrect";
+        res.render("editPassword", { user, message });
+      } else {
+        if (currentPassword != newPassword) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(newPassword, salt);
+          await user.save();
+
+          const message = "password updated successfully";
+          res.render("editPassword", { user, message });
+        } else {
+          const message = "new password cannot be the same as current password";
+          res.render("editPassword", { user, message });
+        }
+      }
+    }
+  } else {
+    const message = "new password and confirm password do not match";
+    res.render("editPassword", { user, message });
+  }
+});
+
+module.exports = router;
